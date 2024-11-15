bd7b7436faf8f43e2763d7751bbec822f015a6d2
tamanna1809 <tamanna.23bsc10133@sst.scaler.com>
2024-11-15 17:15:17 +0530

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
-app.listen(3001, () => {
-  console.log("Server is running at port 3001!!");
+app.use(express.json());
+
+app.listen(3002, () => {
+  console.log("Server is running at port 3002!");
 });
